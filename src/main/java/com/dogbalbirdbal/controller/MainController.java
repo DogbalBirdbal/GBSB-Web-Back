db8c5c93ac70ff20cd3542f5e7ede91562be8cf6
kimjuyoung <dudwnrla00@ajou.ac.kr>
2022-11-12 18:18:16 +0900

    [+] 22.11.12
    - 회원가입 페이지에서 name, id, password, email db 테이블에 삽입

--- src/main/java/com/dogbalbirdbal/controller/MainController.java
+++ src/main/java/com/dogbalbirdbal/controller/MainController.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Properties;
 
 @RestController
 public class MainController {
@@ -29,11 +34,38 @@ public class MainController {
         return stringStringHashMap;
     }
 
-    @GetMapping("register/{id}/{password}")
-    public HashMap<String, String> registerController(@PathVariable String id, @PathVariable String password, Model model) {
+    @GetMapping("register/{name}/{id}/{password}/{email}")
+    public HashMap<String, String> registerController(@PathVariable String name,
+                                                      @PathVariable String id,
+                                                      @PathVariable String password,
+                                                      @PathVariable String email,
+                                                      Model model) {
         HashMap<String, String> stringStringHashMap = new HashMap<>();
+        String url = "jdbc:postgresql://127.0.0.1:5432/wheretogo";
+        String user = "2";
+        String password1 = "1!"; //password 입력
+        try{
+            Connection connect = null;
+            connect = DriverManager.getConnection(url, user, password1);
+            String sql = "insert into MyUser(name, id, password, email) values(?, ?, ?, ?)";
+            PreparedStatement p = connect.prepareStatement(sql);
+            p.setString(1, name);
+            p.setString(2, id);
+            p.setString(3, password);
+            p.setString(4, email);
+            p.executeUpdate();
+
+            stringStringHashMap.put("Result", "Success");
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            stringStringHashMap.put("Result", "fail");
+        }
+
+        stringStringHashMap.put("name", name);
         stringStringHashMap.put("ID", id);
         stringStringHashMap.put("password", password);
+        stringStringHashMap.put("email", email);
 
         //TODO DB 연동해서
         //TODO  회원가입이 성공하면 어떤 페이지를 보여줌
