e1aba0f490b645cdde95f16599573dd0ec4fce7d
kimjuyoung <dudwnrla00@ajou.ac.kr>
2022-11-12 15:31:31 +0900

    변경

--- src/main/java/com/dogbalbirdbal/coreserver/CoreServerApplication.java
+++ src/main/java/com/dogbalbirdbal/coreserver/CoreServerApplication.java
@@ -8,6 +8,7 @@ public class CoreServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CoreServerApplication.class, args);
+        System.out.println("테스트!");
     }
 
 }
